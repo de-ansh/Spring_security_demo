0480d0dd3640f90672a358d50ed86de8fedccd5a
Ashish <anshm9448@gmail.com>
2023-04-21 01:18:29 +0530

    Complete

--- springsecurityDemo/src/main/java/com/example/springsecurityDemo/config/WebSecurityConfig.java
+++ springsecurityDemo/src/main/java/com/example/springsecurityDemo/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springsecurityDemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,30 +13,33 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class WebSecurityConfig   {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    //25:38
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails normalUser= User
+       /* UserDetails normalUser= User
                 .withUsername("Pythonbot")
                 .password(passwordEncoder().encode("12345678"))
-                .roles("Role_normal")
+                .roles("Normal")
                 .build();
         UserDetails AdminUser = User
-                .withUsername("Admin")
+                .withUsername("admin")
                 .password(passwordEncoder().encode("12345678"))
-                .roles("Role_admin")
+                .roles("Admin")
                 .build();
         UserDetails ActiveUser = User
                 .withUsername("Active")
                 .password(passwordEncoder().encode("12345678"))
-                .roles("Role_active")
+                .roles("Active")
                 .build();
-        return new InMemoryUserDetailsManager(normalUser,ActiveUser,AdminUser);
+        return new InMemoryUserDetailsManager(normalUser,ActiveUser,AdminUser);*/
+        return  new CustomUserDetailsService;
     }
 
 
@@ -44,8 +48,12 @@ public class WebSecurityConfig   {
 
         httpSecurity.csrf().disable().
                 authorizeHttpRequests()
-                .requestMatchers("/api/v1/docs/normal")
-                .permitAll().anyRequest().authenticated().and().formLogin();
+              //  .requestMatchers("/api/v1/docs/admin").
+              //  hasRole("Admin")
+                //.requestMatchers("/api/v1/docs/active").hasRole("Active")
+               // .requestMatchers("/api/v1/docs/normal")
+             //   .permitAll()
+                .anyRequest().authenticated().and().formLogin();
         return httpSecurity.build();
     }
 }
--- springsecurityDemo/src/main/java/com/example/springsecurityDemo/controller/HomeController.java
+++ springsecurityDemo/src/main/java/com/example/springsecurityDemo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.springsecurityDemo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/docs")
 public class HomeController {
 
+    @PreAuthorize("hasRole('Normal')")
     @GetMapping("/normal")
     public ResponseEntity<String>normaluser(){
         return ResponseEntity.ok("yes, Its a normal User");
     }
 
 
+    @PreAuthorize("hasRole('Active')")
     @GetMapping("/home")
     public ResponseEntity<String>home(){
         return ResponseEntity.ok("this is Home Page");
     }
 
+    @PreAuthorize("hasRole('Admin')")
     @GetMapping("/admin")
     public ResponseEntity<String>admin(){
-        return ResponseEntity.ok("this is Home Page");
+        return ResponseEntity.ok("this is ADMIN Page");
     }
 }
